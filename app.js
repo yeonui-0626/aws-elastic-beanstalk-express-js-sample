56134e392b9073ffe5de3fe1dfe0bb7918f8d622
이연의 <dusdml1502@naver.com>
2023-07-31 22:27:42 +0900

    edit1

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-진짜 치사하다
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => res.send("Hello Potato!!!!!!!!!!!!!!!!!!!"));
+
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
